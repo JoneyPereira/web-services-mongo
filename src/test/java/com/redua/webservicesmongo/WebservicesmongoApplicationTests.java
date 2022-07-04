e2971a4cf5482ddcb7d7f601f04366ca9d18d279
Joney Pereira <joney.pereira.ext@conductor.com.br>
2022-07-04 16:19:16 -0300

    Criado projeto SpringBoot.

---
+++ src/test/java/com/redua/webservicesmongo/WebservicesmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redua.webservicesmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
