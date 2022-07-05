da61c2b8f69a364f9659936bd88be1398b4d5bcd
Joney Pereira <joney.pereira.ext@conductor.com.br>
2022-07-05 09:40:19 -0300

    Criado o recurso de inserção de um usuario.

--- src/main/java/com/redua/webservicesmongo/resources/UserResource.java
+++ src/main/java/com/redua/webservicesmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.redua.webservicesmongo.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,8 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.redua.webservicesmongo.domain.User;
 import com.redua.webservicesmongo.dto.UserDTO;
@@ -37,5 +41,15 @@ public class UserResource {
        User obj = service.findById(id);
 
         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){
+
+        User obj = service.fromDTO(objDTO);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/redua/webservicesmongo/services/UserService.java
+++ src/main/java/com/redua/webservicesmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.redua.webservicesmongo.domain.User;
+import com.redua.webservicesmongo.dto.UserDTO;
 import com.redua.webservicesmongo.repository.UserRepository;
 import com.redua.webservicesmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
